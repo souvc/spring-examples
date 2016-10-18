58981484e9e3fcd4bb7e36664701f4e30aa4948b
souvc <291117974@qq.com>
2016-10-18 14:23:41 +0800

    Spring3 + Quartz 1.8.6 scheduler example

---
+++ spring-examples/Chapter34-1-2/src/main/java/com/souvc/spring/App.java
@@ -0,0 +1,17 @@
+package com.souvc.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext applicationContext=new ClassPathXmlApplicationContext("Spring-Quartz.xml");
+    	System.out.println(applicationContext);
+    }
+}
---
+++ spring-examples/Chapter34-1-2/src/main/java/com/souvc/spring/RunMeTask.java
@@ -0,0 +1,9 @@
+package com.souvc.spring;
+
+public class RunMeTask {
+	
+	public void printMe() {
+		System.out.println("Spring 3 + Quartz 1.8.6 ~");
+	}
+	
+}
