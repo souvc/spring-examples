d11c413b4534bbbf332b6d9fae0385d5c78dc6ea
souvc <291117974@qq.com>
2016-11-26 21:49:17 +0800

    Spring通过Type属性进行自动注入(Spring-AutoWiring-by-Type)

---
+++ spring-examples/Chapter5-4-1-4/src/main/java/com/souvc/spring/App.java
@@ -0,0 +1,13 @@
+package com.souvc.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+
+		Person person = (Person) context.getBean("person");
+		System.out.println(person);
+	}
+}
---
+++ spring-examples/Chapter5-4-1-4/src/main/java/com/souvc/spring/Person.java
@@ -0,0 +1,20 @@
+package com.souvc.spring;
+ 
+public class Person 
+{
+	private Ability ability;
+
+	public Ability getAbility() {
+		return ability;
+	}
+
+	public void setAbility(Ability ability) {
+		this.ability = ability;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [ability=" + ability + "]";
+	}
+
+}
