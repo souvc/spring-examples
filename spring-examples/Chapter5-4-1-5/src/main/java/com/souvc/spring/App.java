bdc3a40e389553e9e9203be2df3e3a51cb2f059e
souvc <291117974@qq.com>
2016-11-26 22:08:43 +0800

    Spring通过Constructor进行自动注入(Spring-AutoWiring-by-Constructor)

---
+++ spring-examples/Chapter5-4-1-5/src/main/java/com/souvc/spring/App.java
@@ -0,0 +1,13 @@
+package com.souvc.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+
+		Developer developer = (Developer) context.getBean("developer");
+		System.out.println(developer);
+	}
+}
