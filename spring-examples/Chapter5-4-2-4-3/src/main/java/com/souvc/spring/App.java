45038e04e150becc40ee65b6cecfb4e4a8d3c8d3
souvc <291117974@qq.com>
2016-11-28 08:07:49 +0800

    Map集合

---
+++ spring-examples/Chapter5-4-2-4-3/src/main/java/com/souvc/spring/App.java
@@ -0,0 +1,14 @@
+package com.souvc.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+
+		Customer cust = (Customer) context.getBean("CustomerBean");
+		System.out.println(cust);
+		
+	}
+}
